93daadeda0e8dcc79b16f0ccdbce535458e7e127
RinaSasaki <rinasasaki@example.com>
2024-03-29 05:12:08 +0000

    管理画面デザインにBootstrap5を追加

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -4,16 +4,16 @@
 @section('contents')
         <h1>管理画面　ログイン</h1>
         @if ($errors->any())
-        <div>
+            <div>
             @foreach ($errors->all() as$error)
                 {{ $error }}<br>
             @endforeach
-        </div>
+            </div>
         @endif
         <form action="/admin/login" method="post">
             @csrf
             ログインID：<input name="login_id" value="{{ old('login_id') }}"><br>
             パスワード：<input name="password" type="password"><br>
-            <button>ログインする</button>
+            <button class="btn btn-primary mb-3">ログインする</button>
         </form>
 @endsection
